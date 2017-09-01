b3089eedf8d27e69af43dd26136b848c90770137
Zhenyu <zhenyu051988@gmail.com>
2017-09-01 13:03:49 -0700

    Finishe the methods of url manager

--- WebCrawlerBeginner/baike_spider/url_manager.py
+++ WebCrawlerBeginner/baike_spider/url_manager.py
@@ -7,21 +7,35 @@ Created on 2017Äê8ÔÂ31ÈÕ
 
 class UrlManager(object):
     
+    def __init__(self):
+        self.new_urls = set()
+        self.old_urls = set()
     
-    def add_new_url(self):
-        pass
+    #add one new url to manager
+    def add_new_url(self, url):
+        if url is None:
+            return
+        if url not in self.new_urls and url not in self.old_urls:
+            self.new_urls.add(url)
 
-    
+    #add some new urls to manager
+    def add_new_urls(self, urls):
+        if urls is None or len(urls)==0:
+            return
+        for url in urls:
+            self.add_new_url(url)
+
+    #if the manager has new url
     def has_new_url(self):
-        pass
+        return len(self.new_urls)!=0
 
-    
+    #get a new url to craw from manager
     def get_new_url(self):
-        pass
+        new_url = self.new_urls.pop()
+        self.old_urls.add(new_url)
+        return new_url
 
-    
-    def add_new_urls(self):
-        pass
+
     
     
     
