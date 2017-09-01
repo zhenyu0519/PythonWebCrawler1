f2352790fe3e4cd5fa6343c0abc7c48d3db09290
Zhenyu <zhenyu051988@gmail.com>
2017-09-01 13:50:12 -0700

    finish the html downloader

--- WebCrawlerBeginner/baike_spider/html_downloader.py
+++ WebCrawlerBeginner/baike_spider/html_downloader.py
@@ -1,15 +1,22 @@
 '''
 Created on 2017Äê8ÔÂ31ÈÕ
 
-@author: NUC
+@author: Jeffery
 '''
-
+import urllib2
 
 class HtmlDownloader(object):
     
     
-    def download(self):
-        pass
+    def download(self,url):
+        if url is None:
+            return None;
+        response= urllib2.urlopen(url)
+        
+        if response.getcode()!=200:
+            return None;
+        
+        return response.read()
     
     
 
