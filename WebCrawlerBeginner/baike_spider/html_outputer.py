6f2b60f2d5b6f50ff4aead51f3bf35ff92ab9829
Zhenyu <zhenyu051988@gmail.com>
2017-09-01 14:21:23 -0700

    outputer is done

--- WebCrawlerBeginner/baike_spider/html_outputer.py
+++ WebCrawlerBeginner/baike_spider/html_outputer.py
@@ -7,13 +7,35 @@ Created on 2017Äê8ÔÂ31ÈÕ
 
 class HtmlOutputer(object):
     
+    def __init__(self):
+        self.datas =[]
     
-    def collect_data(self):
-        pass
+    
+    def collect_data(self, data):
+        if data is None:
+            return
+        self.datas.append(data)
 
     
     def output_html(self):
-        pass
+        fout = open('output.html', 'w')
+        
+        fout.write("<html>")
+        fout.write("<body>")
+        
+        fout.write("<table>")
+        
+        for data in self.datas:
+            fout.write("<tr>")
+            fout.write("<td>%s</td>"%data['url'])
+            fout.write("<td>%s</td>"%data['title'].encode('utf-8'))
+            fout.write("<td>%s</td>"%data['summary'])
+            fout.write("</tr>")
+        
+        fout.write("</table>")
+        
+        fout.write("</body>")
+        fout.write("</html>")
     
     
     
